447a8ee7e7e9bd079493a62c64eabd98f063cdc1
Andria Ghurtchumelia <andriaghurchumelia4@gmail.com>
2024-12-09 16:45:01 +0400

    feat_SECTION_DONATION

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-let vaxo = "gujikela";
-console.log(vaxo);
+
+const completedDonation = document.getElementById("completedDonation")
+const exploreAllBtn = document.getElementById("exploreAllBtn")
+const cardDonation = document.getElementById("cardDonation")
+const donationCardMainContainer = document.getElementById("donationCardMainContainer")
+completedDonation.classList.add("complated_donation")
+async function getApi() {
+    let data = await fetch('https://fakestoreapi.com/products')
+    let products = await data.json()
+
+}
+
+getApi()
+
+exploreAllBtn.addEventListener("click" , () =>{
+    donationCardMainContainer.lastChild.classList.toggle("card_remove")
+})
