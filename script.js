44747f4d204f26d99b38c5d457465878a3c5ec37
Andria Ghurtchumelia <andriaghurchumelia4@gmail.com>
2024-12-19 20:30:00 +0400

    feat(SECOND_SECTION.HTML)
    add donation card javascript function and ADD charity help section

--- script.js
+++ script.js
@@ -17,41 +17,44 @@ async function getApi() {
       // ეს არის განხორციელებული დონაციის პროცენტულობა 
       let donationLevel = (product.price1 / product.price2) * 100;
       // დივის სიგრძეს ვაძლევ ამ პროცენტულობას,  შესაბამისად ზომას თავისით აძლევს
-
-        // იქმნება დონაციის ქარდები 
-        div.innerHTML =
-         `<div class="donate_card">
-                <div class="donate_card_picture">
-                  <img src="${product.img}" alt="" >
-                  <div class="donate_card_picture_meaning"><p>${product.img_meaning}</p></div>
-                </div>
-                <div class="donate_card_description">
+      
+      // იქმნება დონაციის ქარდები 
+      div.innerHTML =
+      `<div class="donate_card">
+      <div class="donate_card_picture">
+      <img src="${product.img}" alt="" >
+      <div class="donate_card_picture_meaning"><p>${product.img_meaning}</p></div>
+      </div>
+      <div class="donate_card_description">
                   <h2>${product.description}</h2>
                   <div class="donate_card_donation" id="cardDonation">
-                    <p class="completed_donation" >${product.price1}$</p>
-                    <p class="goal_donation" > $ ${product.price2}</p>
+                  <p class="completed_donation" >${product.price1}$</p>
+                  <p class="goal_donation" > $ ${product.price2}</p>
                   </div>
                   <div class="donate_card_level">
-                    <div class="completed_donation_level" style="width : ${donationLevel}%"></div>
+                  <div class="completed_donation_level" style="width : ${donationLevel}%"></div>
                   </div>
-                  // <p class="full_donate_text">Goal Achieved</p>
                   <button class="donate_btn "  >Make a Donation</button>
-                </div>
-              </div>`      
-      
-          // let donationBtn = div.querySelector(".donate_btn")
-          // let completedDonation = div.querySelector(".completed_donation")
-          // let completedDonationLevel = div.querySelector(".completed_donation_level")
-          // let fullDonateText  = div.querySelector("full_donate_text")
-          // donationBtn.addEventListener("click" , ()=>{
-          //   product.price1+=100
-          //   completedDonation.textContent = `${product.price1}$`
+                  </div>
+                  </div>`      
+                  
+          let donationBtn = div.querySelector(".donate_btn")
+          let completedDonation = div.querySelector(".completed_donation")
+          let completedDonationLevel = div.querySelector(".completed_donation_level")
+          let fullDonateText  = div.querySelector("full_donate_text")
+          donationBtn.addEventListener("click" , ()=>{
+            product.price1+=100
+            completedDonation.textContent = `${product.price1}$`
             
-          //   let newDonationFormula = (product.price1 / product.price2) * 100;
-          //   completedDonationLevel.style.width =`${newDonationFormula}%`
+            let newDonationFormula = (product.price1 / product.price2) * 100;
+            completedDonationLevel.style.width =`${newDonationFormula}%`
             
-            
-          // })
+            if(product.price1 >= 10000){
+              donationBtn.setAttribute("disabled", false)
+              completedDonationLevel.style.width = "100%"
+              completedDonation.textContent = "10000$"
+            }
+          })
         hiddenCardsContainer.appendChild(div)
     })
 }
